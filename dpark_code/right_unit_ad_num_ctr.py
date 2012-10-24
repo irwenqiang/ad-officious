74bd2c5217b6f0121ed6d1b231143d1a97abe10d
irwenqiang <605506205@qq.com>
2012-10-24 17:04:17 +0800

    use mapValue to compute imp and click

--- dpark_code/right_unit_ad_num_ctr.py
+++ dpark_code/right_unit_ad_num_ctr.py
@@ -64,6 +64,18 @@ def flat_map_unit_type_priority(line):
 	for i in xrange(2, len(line)):
 		yield [(line[0],line[1],line[i][0]),line[i][1:]]
 
+def map_value_unit_type_priority(line):
+
+	#['1', '1', '0'] 
+	cluster = 0
+	imp = 0
+	click = 0
+	for i in xrange(len(line)):
+		cluster = int(line[i][0])
+		imp += int(line[i][1])
+		click += int(line[i][2])
+	
+	return [cluster, imp, click, float(click) / imp]
 
 def group_unit_type_priority(line):
 
@@ -82,112 +94,76 @@ def main(txt, infile, outfile):
 
 	txt_rdd = ctx.textFile(txtfilename)
 
-	t = time()
 	txt_rdd = txt_rdd.map(divide_txt)
 	#('5988', ['2', 'CPM']) 
-	print "txt_rdd map finish"
-	print time() - t
 
-	csv_rdd = ctx.textFile(csvfilename) 
-	t = time()
+	csv_rdd = ctx.textFile(csvfilename, splitSize=64<<20) 
 	#print csv_rdd.take(100)
 	csv_rdd = csv_rdd.filter(remove_some_bid_unitid)
-	print "csv_rdd filter finish"
-	print time() - t
-	t = time()
 
 	csv_rdd = csv_rdd.map(divide_csv)	
 	#('6379', ['-1', '1236054964187470000', '6379', '77', '1', '1', '0'])
-	print "csv_rdd map finish"
-	print time() - t
 
-	t = time()
 	record_rdd = txt_rdd.join(csv_rdd)
-	print "txt_rdd join csv_rdd finish"
 	#('6370', (['2', 'COMPLEMENT'], ['-1', '8183016859528920000', '6370', '86', '3', '1', '0']))
-	print time() - t
-	
-	t = time()
 	record_rdd = record_rdd.mapValue(join_element)
 	#('6370', ['2', 'COMPLEMENT', '-1', '8183016859528920000', '6370', '86', '3', '1', '0']) 
-	print "record_rdd mapValue finish"
-	print time() - t
-
-	t = time()
 	record_rdd = record_rdd.groupBy(lambda line : str(line[1]).split()[5] + str(line[1]).split()[1])
-	print record_rdd.take(3)
-	print "record_rdd groupBy finish"
-	print time() - t
-
-	t = time()
+	#print record_rdd.take(3)
 	record_rdd = record_rdd.map(map_unit_type)
-	print record_rdd.take(10)
+	#print record_rdd.take(10)
 	record_rdd = record_rdd.flatMap(flat_map_unit_type_priority)
 	print "8" * 50
 	print record_rdd.count()
-	print record_rdd.take(5)
+	#print record_rdd.take(5)
 	#record_rdd = record_rdd.groupBy(group_unit_type_priority)
 	record_rdd = record_rdd.groupByKey()
+	#print "*" * 50
+	#print record_rdd.take(5)
 
-	print "*" * 50
-	print record_rdd.take(100)
-	print "record_rdd reduceByKey"
-	record = record_rdd.collectAsMap()
-	#(('86', 'CPM', '1'), [['2', '1', '0'], ['3', '1', '0']])
-	print "chenwq " * 10
-	print record.keys()
+	record_rdd = record_rdd.mapValue(map_value_unit_type_priority)
+	#print "#" * 50
+	#print record_rdd.take(5)
+	results = record_rdd.collect()
+	#print "result"
 	
-	print "write back"
-	print "$" * 50
-	out = file(outfile, "w")	
+	out = file(outfile, 'w')
 
-	for key, value in record.items():
-		result = [0 for i in xrange(3)]
-
-		out.write(str(key))
+	for i in xrange(len(results)):
+		out.write(results[i][0][0])
+		out.write("\t")
+		out.write(results[i][0][1])
+		out.write("\t")
+		out.write(results[i][0][2])
+		out.write("\t")
+		out.write(str(results[i][1][0]))
+		out.write("\t")
+		out.write(str(results[i][1][1]))
+		out.write("\t")
+		out.write(str(results[i][1][2]))
+		out.write("\t")
+		out.write(str(results[i][1][3]))
 		out.write("\n")
-		print key
-		'''
-		[['1', '1', '0'], ['2', '1', '0']]
-		[['1', '1', '0']]
-		[['2', '1', '0'], ['3', '1', '0']]
-		[['1', '1', '0']]
-		[['1', '1', '0'], ['1', '1', '0'], ['3', '1', '0']]
-		[['1', '1', '0']]
-		'''
-		for i in xrange(len(value)):
-			result[0] += int(value[i][1])
-			result[1] += int(value[i][2])
-
-
-		result[2] = float(result[1]) / result[0]
-		print result
-		out.write(str(result))
-		out.write("\n")
-	
 	out.close()
-
-	print "record_rdd map finish"
-	print time() - t
 	
 if __name__ == "__main__":
 
-	#txt = "/home/cwq_intern/Otc/code/bi/a"
+	txt = "/home/cwq_intern/Otc/code/bi/a"
 	#datadir = "/home/cwq_intern/Otc/code/bi/tools/data"
-	#datadir = "/data/ereborlog/"
+	datadir = "/data/ereborlog/2012-10-01/"
 
 	#print filename
-	#main(txt, datadir, "result/" + "t")
+	main(txt, datadir, "results/" + "t")
 	
-	txt = "/home/cwq_intern/Otc/code/bi/a"
-	datadir = "/data/ereborlog"
-	for idir in os.listdir(datadir):
-		 
-		if len(idir) == 10:
-			print idir
-			main(txt, datadir + "/" + idir + "/", "results/" + idir)
+#	txt = "/home/cwq_intern/Otc/code/bi/a"
+#	datadir = "/data/ereborlog"
+#	cnt = 0
+#	for idir in os.listdir(datadir):
+#			 
+#		if len(idir) == 10:
+#			cnt += 1
+#			if cnt == 2:
+#				break
+#			print idir
+#			main(txt, datadir + "/" + idir + "/", "results/" + idir)
 
-			#for filename in os.listdir(datadir + "/" + idir):
-			#	print idir + "-" + filename
-			#	main(txt, datadir + "/" + idir + "/" + filename, "results/" + idir + "-" + filename)
-		
